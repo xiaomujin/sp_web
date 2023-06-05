4eba37473ed51e3c6f73e53cb5ada3eaf2dcafa9
KuroNeko <1419229777@qq.com>
2023-06-05 17:59:17 +0800

    update: 日志多layer输出

--- src/config/log.rs
+++ src/config/log.rs
@@ -1,7 +1,10 @@
 use time::macros::format_description;
-use tracing::Level;
 use tracing_appender::non_blocking::WorkerGuard;
+use tracing_subscriber::{Layer};
+use tracing_subscriber::filter::LevelFilter;
 use tracing_subscriber::fmt::time::{LocalTime};
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
 
 pub fn init_log() -> WorkerGuard {
     // 如果不是在main函数中，guard必须返回到main()函数中，否则不输出任何信息到日志文件
@@ -13,38 +16,26 @@ pub fn init_log() -> WorkerGuard {
         .with_level(true)
         .with_target(true)
         .with_thread_names(true)
-        // .with_source_location(true)
+        .with_line_number(true)
         .with_timer(timer);
 
-    // 初始化并设置日志格式(定制和筛选日志)
-    #[cfg(debug_assertions)]
-    {
-        tracing_subscriber::fmt()
-            .with_max_level(Level::DEBUG)
-            .with_writer(std::io::stdout) // 写入标准输出
-            // .with_writer(non_blocking) // 写入文件，将覆盖上面的标准输出
-            .with_ansi(true)  // 如果日志是写入文件，应将ansi的颜色输出功能关掉
-            .event_format(format)
-            .init();
-        tracing::info!("Debug mode!");
-    }
+    let std_writer = tracing_subscriber::fmt::layer()
+        .with_writer(std::io::stdout) // 写入标准输出
+        .with_ansi(true)  // 如果日志是写入文件，应将ansi的颜色输出功能关掉
+        .event_format(format.clone())
+        .with_filter(LevelFilter::DEBUG);
 
-    #[cfg(not(debug_assertions))]
-    {
-        tracing_subscriber::fmt()
-            .with_max_level(Level::WARN)
-            // .with_writer(std::io::stdout)
-            .with_writer(non_blocking)
-            .with_ansi(false)
-            .event_format(format)
-            .init();
-        tracing::info!("Release mode!");
-    }
+    let file_writer = tracing_subscriber::fmt::layer()
+        .with_writer(non_blocking)
+        .with_ansi(false)
+        .event_format(format)
+        .with_filter(LevelFilter::INFO);
 
-    tracing::trace!("tracing-trace");
-    tracing::debug!("tracing-debug");
-    tracing::info!("tracing-info");
-    tracing::warn!("tracing-warn");
-    tracing::error!("tracing-error");
+    tracing_subscriber::registry()
+        .with(std_writer)
+        .with(file_writer)
+        .init();
+
+    tracing::debug!("Tracing initialized.");
     _guard
 }
