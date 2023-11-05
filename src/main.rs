485a5fb3c8dc222ef17ca33f0432cf72437c04d7
KuroNeko <1419229777@qq.com>
2023-11-05 19:53:20 +0800

    update: 监听ipv6

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::net::{Ipv4Addr, Ipv6Addr, SocketAddrV4, SocketAddrV6};
 use serde::{Deserialize, Serialize};
 use clap::Parser;
 use salvo::{Listener, Router, Server};
@@ -8,8 +9,8 @@ use sp_web::config::log;
 #[derive(Parser, Debug, Deserialize, Serialize)]
 struct Args {
     /// 绑定地址
-    #[arg(short, long, default_value = "127.0.0.1:3000")]
-    bind: String,
+    #[arg(short, long, default_value_t = 3000)]
+    port: u16,
 }
 
 #[tokio::main]
@@ -18,10 +19,12 @@ async fn main() {
     let _guard = log::init_log();
 
     let app = Router::new().get(root);
-
-    let addr = TcpListener::new(&args.bind).bind().await;
-    tracing::info!("listening on {}", args.bind);
-    Server::new(addr).serve(app).await;
+    let socket_v4 = SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, args.port);
+    let socket_v6 = SocketAddrV6::new(Ipv6Addr::UNSPECIFIED, args.port, 0, 0);
+    let addr = TcpListener::new(socket_v4).join(TcpListener::new(socket_v6)).bind().await;
+    let server = Server::new(addr).serve(app);
+    tracing::info!("listening on port: {}", args.port);
+    server.await;
 }
 
 
