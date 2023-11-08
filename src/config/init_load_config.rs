30d424d6ef931bc69792e0ecf429a751f8e8d91d
KuroNeko <1419229777@qq.com>
2023-11-08 15:23:50 +0800

    update: 格式化字符串

--- src/config/init_load_config.rs
+++ src/config/init_load_config.rs
@@ -7,7 +7,7 @@ fn load_env_config() -> Option<EnvConfig> {
 }
 
 fn load_global_config_from_env(active: String) -> Option<GlobalConfig> {
-    let path = format!("application-{}.yml", active);
+    let path = format!("application-{active}.yml");
     load_config::<GlobalConfig>(&path)
 }
 
@@ -19,7 +19,7 @@ pub fn load_global_config() -> Option<GlobalConfig> {
 }
 
 fn load_config<T>(path: &str) -> Option<T> where T: DeserializeOwned {
-    let cfg_str = std::fs::read_to_string(path).expect(&format!("failure read file {}", path));
+    let cfg_str = std::fs::read_to_string(path).expect(&format!("failure read file {path}"));
     match serde_yaml::from_str::<RootSchema>(&cfg_str) {
         Ok(root_schema) => {
             let data = serde_json::to_string_pretty(&root_schema).expect("");
