785c4a02ae68c4b8502569ea0fc8d446757fd9b7
KuroNeko <1419229777@qq.com>
2024-04-03 18:30:25 +0800

    update: 接口日志

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::net::{Ipv4Addr, Ipv6Addr, SocketAddrV4, SocketAddrV6};
 use serde::{Deserialize, Serialize};
 use clap::Parser;
-use salvo::{Listener, Router, Server};
+use salvo::{Listener, Router, Server, Service};
+use salvo::logging::Logger;
 use salvo::prelude::{TcpListener};
 use sp_web::config::{log};
 use sp_web::constant::app;
@@ -23,8 +24,9 @@ async fn main() {
     let port = args.port.unwrap_or(config.server.port);
     // tracing::info!("try on port: {}", port);
     let router = controller::init();
+    let service = Service::new(router).hoop(Logger::new());
     // let server = run_server(port, router);
-    let server = start_server_with_port(port, router);
+    let server = start_server_with_port(port, service);
     tracing::info!("listening on port: {}", port);
     server.await;
 }
@@ -44,22 +46,22 @@ async fn run_server(port: u16, router: Router) {
     Server::new(addr).serve(router).await;
 }
 
-async fn start_server_with_port(port: u16, router: Router) {
+async fn start_server_with_port(port: u16, service: Service) {
     tracing::info!("try on port: {}", port);
     let socket_v4 = SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, port);
     let socket_v6 = SocketAddrV6::new(Ipv6Addr::UNSPECIFIED, port, 0, 0);
     match TcpListener::new(socket_v4).join(TcpListener::new(socket_v6)).try_bind().await {
         Ok(addr) => {
-            Server::new(addr).serve(router).await;
+            Server::new(addr).serve(service).await;
         }
         Err(_) => {
             match TcpListener::new(socket_v6).try_bind().await {
                 Ok(addr) => {
-                    Server::new(addr).serve(router).await;
+                    Server::new(addr).serve(service).await;
                 }
                 Err(_) => {
                     let addr = TcpListener::new(socket_v4).bind().await;
-                    Server::new(addr).serve(router).await;
+                    Server::new(addr).serve(service).await;
                 }
             }
         }
